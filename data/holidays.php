fd1ef0a3c28cfa9db0affad5e88f2ac6bd89d872
Jonas Markussen <jonassm@ifi.uio.no>
2015-05-12 14:35:33 +0200

    HolidaysIterator in a separate file

--- data/holidays.php
+++ data/holidays.php
@@ -1,6 +1,6 @@
 <?
 namespace data;
-use \Iterator;
+use data\HolidaysIterator;
 
 /*
  * Keep track of holidays
@@ -160,51 +160,6 @@ final class Holidays
         }
 
         return new HolidaysIterator($this->aggregated);
-    }
-}
-
-final class HolidaysIterator implements Iterator
-{
-    private $aggregated;
-    private $keys;
-    private $current;
-    private $count;
-
-    public function __construct(&$holidays)
-    {
-        $this->aggregated = &$holidays;
-        $this->keys = array_keys($this->aggregated);
-        $this->count = count($this->aggregated);
-    }
-
-    public function rewind()
-    {
-        $this->current = 0;
-    }
-
-    public function current()
-    {
-        return $this->aggregated[$this->keys[$this->current]];
-    }
-
-    public function key()
-    {
-        if ($this->current < $this->count)
-        {
-            return $this->keys[$this->current];
-        }
-
-        return null;
-    }
-
-    public function next()
-    {
-        ++$this->current;
-    }
-
-    public function valid()
-    {
-        return $this->current < $this->count;
     }
 }
 
---
+++ data/holidaysiterator.php
@@ -0,0 +1,50 @@
+<?
+namespace data;
+use \Iterator;
+
+final class HolidaysIterator implements Iterator
+{
+    private $aggregated;
+    private $keys;
+    private $current;
+    private $count;
+
+    public function __construct(&$holidays)
+    {
+        $this->aggregated = &$holidays;
+        $this->keys = array_keys($this->aggregated);
+        $this->count = count($this->aggregated);
+    }
+
+    public function rewind()
+    {
+        $this->current = 0;
+    }
+
+    public function current()
+    {
+        return $this->aggregated[$this->keys[$this->current]];
+    }
+
+    public function key()
+    {
+        if ($this->current < $this->count)
+        {
+            return $this->keys[$this->current];
+        }
+
+        return null;
+    }
+
+    public function next()
+    {
+        ++$this->current;
+    }
+
+    public function valid()
+    {
+        return $this->current < $this->count;
+    }
+}
+
+?>
