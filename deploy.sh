52b2801bf34fc96b702e45f95415728d1c3be7d3
Jonas Markussen <jonassm@ifi.uio.no>
2015-09-23 16:09:06 +0200

    chmod in deployscript

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,15 @@ rm -f .htaccess
 for file in `find ./ -type l -name "*.phps"`; do
 	rm $file
 done
+rm -f README
 
 ln -s htaccess .htaccess
 for file in `find ./ -type f -name "*.php"`; do
 	newname="$file"s
 	ln -s `basename $file` $newname
 done
+ln -s README.md README
+
+chmod 644 README.md
+chmod 644 LICENSE
 
